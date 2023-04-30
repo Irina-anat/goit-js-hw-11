14b651c36ed45b6e0fa5b22204b82d1ce74fcee3
Iryna Rozmaita <iriszechka@ukr.net>
2023-04-30 15:59:04 +0300

    + loadMore

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-import { getImages } from "./js/getImages";
+import { getImages, page} from "./js/getImages";
 import {createMarcup} from "./js/createMarcup"
 
-const searchQuery = document.querySelector('input[name="searchQuery"]');
+//const searchQuery = document.querySelector('input[name="searchQuery"]');
 //console.log(searchQuery)
 const form = document.querySelector('.search-form');
 //console.log(form)
@@ -18,55 +18,59 @@ const gallery = document.querySelector(`.gallery`);
 //console.log(searchBtn);
 const loadMoreBtn = document.querySelector(`.load-more`);
 //console.log(loadMoreBtn)
-let searchValue = ``;
+let searchQuery = ``;
+let currentPage = 1;
 form.addEventListener(`submit`, onSearch);
-loadMoreBtn.addEventListener(`click`, onClickLoadMore);
+loadMoreBtn.addEventListener(`click`, onLoadMore);
 
 async function onSearch(evn) {
   evn.preventDefault()
-   searchValue = searchQuery.value.trim()
-  if (!searchValue) {
+   searchQuery = evn.currentTarget.elements.searchQuery.value.trim() //searchQuery.value.trim() можна буде отримати значення, коли ф-я в ін файлі
+  if (!searchQuery) {
     clear();
      Notify.failure("Please fill in the search field.")
     return;
   }
-  //console.log(searchValue)
+  //console.log(searchQuery)
   else {
     try {
-      const data = await getImages(searchValue);
-      if (data.hits <= 1) { 
+      const data = await getImages(searchQuery);
+      if (data.hits <= 1) {
         clear()
-        Notify.failure("Sorry, there are no images matching your search query. Please try again.") 
+        Notify.failure("Sorry, there are no images matching your search query. Please try again.")
         form.reset()
       } else {
         clear()
         gallery.insertAdjacentHTML('beforeend', createMarcup(data.hits))
         Notify.success(`Hooray! We found ${data.totalHits} images.`)
         const lightbox = new SimpleLightbox('.gallery a', {
-       captionsData: 'alt',
-       captionDelay: 250,
+          captionsData: 'alt',
+          captionDelay: 250,
         }).refresh();
         form.reset()
-      }     
+      }
     }
-    
     catch (err)
-    { console.log(err) }
+    { console.log('ERROR: ' + `error`) }
   }    
 }
 
-
-function clear() {
+ function clear() {
     gallery.innerHTML = '';
 };
 
-function onClickLoadMore() {
- // console.log(`click`)
-  currentPage += 1;
+async function onLoadMore() {
+  currentPage +=1
+  try {
+    
+    const data = await getImages(searchQuery, currentPage)
+    gallery.insertAdjacentHTML('beforeend', createMarcup(data.hits))
+    
+  }
+  catch {
+   { console.log(err) } 
+  }
 }
-
-
-
 
 
 
--- src/js/getImages.js
+++ src/js/getImages.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 const BASE_URL = "https://pixabay.com/api/";
 const API_KEY = "35791203-a082b793cbab5a6f0440a6e52";
+//const currentPage = 1;
+let page = 0 
 
-async function getImages(searchQuery, page = 1) {
+async function getImages(searchQuery, page = 1) {//, 
   const params = new URLSearchParams({
     key: API_KEY,
     q: searchQuery,
@@ -10,12 +12,12 @@ async function getImages(searchQuery, page = 1) {
     orientation: 'horizontal',
     safesearch: true,
     per_page: 40,
-    page
+  //page
   })
   const URL = `${BASE_URL}?${params}`
-  const images = await axios.get(`${BASE_URL}?${params}`).then(page += 1);
+  const images = await axios.get(`${BASE_URL}?${params}&page=${page}`);//.then(page +=1)
   console.log(images.data)
   return images.data;
 }
 
-export {getImages}
+export {getImages, page}
